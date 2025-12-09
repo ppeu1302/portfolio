d414a20ae95ca1e48339f1ab7c9dec55dde169f4
Pedro <ppeu.jobs@outlook.com>
2025-12-09 15:46:59 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/ppeu1302.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ppeu1302.png" alt="" />
-    <Titulo fontSize={20}>Pedro Moreira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Pedro Moreira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        ppeu1302
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
